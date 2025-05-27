2b8fecf8e9ea62abbcbcb11ec44ce5b99d2fcad1
proman3419 <proman7268@gmail.com>
2025-05-28 00:00:46 +0200

    Create entities with Executor Change cron expressions

--- scenarios/k8s-scaling/run.sh
+++ scenarios/k8s-scaling/run.sh
@@ -3,9 +3,17 @@
 # shellcheck disable=SC2046
 echo "$(date +'%Y-%m-%d %T') start test"
 
+kubectl -n themis-executor port-forward svc/themis-executor-svc 31420:8080 &
 cd "$(dirname "$0")"
-kubectl create namespace scenario-k8s-scaling
-kubectl apply -f manifests/init --namespace scenario-k8s-scaling
+for request_file in init/*.httpbody; do
+    echo "Sending $request_file"
+    request_content=$(cat "$request_file")
+    curl -X POST -H "Content-Type: application/json" -d "$request_content" http://localhost:31420/execute
+    echo ""
+done
+echo "$(date +'%Y-%m-%d %T') waiting for initialization"
+sleep 180
+echo "$(date +'%Y-%m-%d %T') initialized"
 
 curr=0
 for ((i=1; i<=6; i++)); do
--- scenarios/k8s-scaling/tear_down.sh
+++ scenarios/k8s-scaling/tear_down.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-kubectl delete namespace scenario-k8s-scaling
 kubectl scale deploy load-test -n loadtest --replicas=0
+#cd "$(dirname "$0")"
+#kubectl delete -f manifests/init
+# TODO: delete * request
