dbc97099d4613f3581122f74503c5ba55fd3b817
proman3419 <proman7268@gmail.com>
2025-05-28 22:50:51 +0200

    Minor changes

--- scenarios/k8s-scaling/init.sh
+++ scenarios/k8s-scaling/init.sh
@@ -3,13 +3,14 @@
 cd "$(dirname "$0")"
 
 kubectl -n themis-executor port-forward svc/themis-executor-svc 31420:8080 &
-sleep 2
+sleep 5
 
 for request_file in init/*.httpbody; do
-    echo "Sending $request_file"
+    echo "sending $request_file"
     request_content=$(cat "$request_file")
     curl -X POST -H "Content-Type: application/json" -d "$request_content" http://localhost:31420/execute
     echo ""
 done
 
+echo "wait for SockShop to start"
 sleep 180
