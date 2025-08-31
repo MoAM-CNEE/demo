faac16117753c2fd0e663478e5372aef0ac8c918
proman3419 <proman7268@gmail.com>
2025-08-31 14:35:26 +0200

    Update init, tear_down Describe the process of running related overhead scenarios

--- scenarios/amocna-overhead/tear_down.sh
+++ scenarios/amocna-overhead/tear_down.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
+echo "> Cleanup infrastructure"
 kubectl -n themis-executor port-forward svc/themis-executor-svc 31420:8080 &
 sleep 10
-
 curl -X POST http://localhost:31420/execute \
   -H "Content-Type: application/json" \
   -d '{
-    "collectionName": "moam.statemanager",
-    "actionName": "DeleteEntityAction",
+    "collectionName": "openstack",
+    "actionName": "DeleteInstanceAction",
     "params": {
-      "query": "select * from entity"
+      "region": "RegionOne",
+      "namePattern": "^infra-.*"
     }
   }'
 echo ""
+
+echo "> Cleanup containerization"
+kubectl delete namespace test
+
+echo "> Cleanup application"
+kubectl rollout restart deployment dummy-message-service -n dummy-message-service
---
+++ scenarios/overhead/README.md
@@ -0,0 +1,7 @@
+1. Run overhead (keep logs).
+2. Set `prometheus_base_ram_usage_bytes` to the baseline RAM usage of Prometheus.
+3. Set `prometheus_start_scenario_`**moam**`_ram_usage_bytes` to the RAM usage of Prometheus at the time of running the scenario.
+4. Document `MoAM performance` plots.
+5. Run amocna-overhead (keep logs)
+6. Set `prometheus_start_scenario_`**amoca**`_ram_usage_bytes` to the RAM usage of Prometheus at the time of running the scenario.
+7. Document `Comparison with AMoCNA` plots.
